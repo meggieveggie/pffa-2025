95b0a467d536c5b33464ed30a5276f61bdbbc0a9
Meggie Veggie <megs.kamp@icloud.com>
2025-01-17 12:27:46 +0100

    final changes

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,29 +2,15 @@
 const logo = new URL('../assets/main_image.png', import.meta.url).href;
 import { BContainer, BRow, BCol, BButton, BImg } from 'bootstrap-vue-3';
 const pffaLogo = new URL('../assets/pffa_logo.png', import.meta.url).href
+const footerlogo = new URL('../assets/footer.png', import.meta.url).href
 </script>
 
 <template>
-  <b-container fluid>
+  <b-container fluid class="d-flex flex-column min-vh-100 px-0">
   <b-row>
     <b-col xs="12" lg="5" class="mt-5">
       <b-row  class="mt-5">
         <b-img :src="pffaLogo"></b-img>
-        <b-col lg="12" class="mb-3">
-          <BButton 
-          href="#/open_call"
-          variant="light"
-          class="form-button w-auto not-bold move-right"
-          ><span class="linked">Submit</span> your film, performance or workshop
-          </BButton>
-        </b-col>
-        <b-col lg="12" class="mb-3">
-          <BButton 
-          href="https://2024.pornfilmfestival.amsterdam"
-          variant="light"
-          class="form-button w-auto not-bold move-right"
-          ><span class="linked">Explore</span> the 2024 edition of the festival</BButton>
-        </b-col>
 
         <b-col lg="12" class="mb-3">
           <BButton 
@@ -36,10 +22,18 @@ const pffaLogo = new URL('../assets/pffa_logo.png', import.meta.url).href
 
         <b-col lg="12" class="mb-3">
           <BButton 
-          href="#/sponsors" 
+          href="https://docs.google.com/document/d/1H-rFBJYy0RLSHHp4uK9TRXUbPbGeeEriX5n-X_j8ats/edit?tab=t.0" 
           variant="light"
           class="form-button w-auto not-bold move-right"
-          ><span class="linked">Sponsorship</span> info</BButton>
+          ><span class="linked">Accessibility</span>  statement</BButton>
+        </b-col>
+
+        <b-col lg="12" class="mb-3">
+          <BButton 
+          href="mailto:pxxnfilmfestams@gmail.com" 
+          variant="light"
+          class="form-button w-auto not-bold move-right"
+          ><span class="linked">Contact us</span> </BButton>
         </b-col>
       </b-row>
     </b-col>
@@ -47,13 +41,15 @@ const pffaLogo = new URL('../assets/pffa_logo.png', import.meta.url).href
       <b-img fluid :src="logo" class="main-image d-flex align-items-center move-image-right"></b-img>
     </b-col>
   </b-row>
+  <b-img :src="footerlogo"  class="footer-image" fluid></b-img>
 </b-container>
+
 </template>
 
 <style scoped>
 
 .main-image {
-  width: 70%; /* Adjust width as needed */
+  width: 55%; /* Adjust width as needed */
   height: auto; /* Adjust height as needed */
 }
 
@@ -78,5 +74,12 @@ const pffaLogo = new URL('../assets/pffa_logo.png', import.meta.url).href
   .move-image-right{
     margin-left: 50px; /* Adjust the value as needed */
   }
+}
+
+/* Footer styles */
+.footer-image {
+  width: 100%;
+  margin-top: auto;
+  margin-bottom: 0px;
 }
 </style>
